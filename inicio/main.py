cbda138951222631b80a1bd8be5e0b04d3b6f79f
BrunoSantosdeOliveira <84398016+BrunoSantosdeOliveira@users.noreply.github.com>
2024-03-10 19:33:24 -0300

    commit inicial
    commit inicial

---
+++ inicio/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+ #Press"" Double Shift to search everywhere for classes, files, tool windows, actions, and settings
+
+faturamneto=4500
+custo=9000
+
+novas_vendas=1000
+faturamneto= faturamneto + novas_vendas
+custo=8000
+
+print("Faturamento",faturamneto)
+print("C 2usto",custo )
+print("Lucro",faturamneto)
